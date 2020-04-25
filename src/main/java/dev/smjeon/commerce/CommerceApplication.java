e1e724484f1ae44732a4749ccc0cca1a8a774d1f
Seongmo Jeon <oeeen3@gmail.com>
2020-04-25 15:54:08 +0900

    Initial commit

---
+++ src/main/java/dev/smjeon/commerce/CommerceApplication.java
@@ -0,0 +1,13 @@
+package dev.smjeon.commerce;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CommerceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CommerceApplication.class, args);
+	}
+
+}
