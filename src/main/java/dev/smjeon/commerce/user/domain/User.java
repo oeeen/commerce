a67fbe6c7211f6d11591a8c9c333279133d8337b
Seongmo Jeon <oeeen3@gmail.com>
2020-05-03 14:22:40 +0900

    feat: add additional information in user domain

--- src/main/java/dev/smjeon/commerce/user/domain/User.java
+++ src/main/java/dev/smjeon/commerce/user/domain/User.java
@@ -4,21 +4,49 @@ import dev.smjeon.commerce.common.BaseEntity;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 
+import javax.persistence.Column;
 import javax.persistence.Entity;
+import java.time.LocalDate;
 
 @NoArgsConstructor
 @Getter
 @Entity
 public class User extends BaseEntity {
+
+    @Column(nullable = false)
     private String email;
+
+    @Column(nullable = false)
     private String password;
+
+    @Column(nullable = false)
     private String name;
+
+    @Column(nullable = false)
     private String nickName;
+
+    private String gender;
+
+    private int age;
+
+    private LocalDate birthday;
 
     public User(String email, String password, String name, String nickName) {
         this.email = email;
         this.password = password;
         this.name = name;
         this.nickName = nickName;
+    }
+
+    public void updateGender(String gender) {
+        this.gender = gender;
+    }
+
+    public void updateAge(int age) {
+        this.age = age;
+    }
+
+    public void updateBirthday(LocalDate birthday) {
+        this.birthday = birthday;
     }
 }
--- src/test/java/dev/smjeon/commerce/user/domain/UserTest.java
+++ src/test/java/dev/smjeon/commerce/user/domain/UserTest.java
@@ -3,7 +3,10 @@ package dev.smjeon.commerce.user.domain;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 
+import java.time.LocalDate;
+
 import static org.junit.jupiter.api.Assertions.assertDoesNotThrow;
+import static org.junit.jupiter.api.Assertions.assertEquals;
 
 public class UserTest {
     @Test
@@ -15,5 +18,27 @@ public class UserTest {
         String password = "Aa12345!";
 
         assertDoesNotThrow(() -> new User(email, password, name, nickName));
+    }
+
+    @Test
+    @DisplayName("추가 정보로 성별, 연령정보, 생일 정보를 받으면 기존 유저를 업데이트합니다.")
+    void moreInformation() {
+        String gender = "남";
+        int age = 20;
+        LocalDate birthday = LocalDate.of(1992, 6, 16);
+
+        String name = "Seongmo";
+        String nickName = "Martin";
+        String email = "oeeen3@gmail.com";
+        String password = "Aa12345!";
+
+        User user = new User(email, password, name, nickName);
+        user.updateGender(gender);
+        user.updateAge(age);
+        user.updateBirthday(birthday);
+
+        assertEquals(user.getGender(), gender);
+        assertEquals(user.getAge(), age);
+        assertEquals(user.getBirthday(), birthday);
     }
 }
