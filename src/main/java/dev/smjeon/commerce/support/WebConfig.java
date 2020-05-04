f60559c28023982175ae27570a85c48c326838b2
Seongmo Jeon <oeeen3@gmail.com>
2020-05-04 17:30:56 +0900

    chore: add modelmapper

---
+++ src/main/java/dev/smjeon/commerce/support/WebConfig.java
@@ -0,0 +1,21 @@
+package dev.smjeon.commerce.support;
+
+import org.modelmapper.ModelMapper;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+import static org.modelmapper.config.Configuration.AccessLevel.PRIVATE;
+
+@Configuration
+public class WebConfig {
+
+    @Bean
+    public ModelMapper modelMapper() {
+        ModelMapper modelMapper = new ModelMapper();
+        modelMapper.getConfiguration()
+                .setFieldAccessLevel(PRIVATE)
+                .setFieldMatchingEnabled(true);
+
+        return modelMapper;
+    }
+}
