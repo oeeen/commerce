59e12b2e54263b7c2030b70bf16de1b8aeb0b2d2
Seongmo Jeon <oeeen3@gmail.com>
2020-05-20 21:13:42 +0900

    feat: add sign up event listener

---
+++ src/main/resources/static/js/signup.js
@@ -0,0 +1,78 @@
+const SignUpApp = (() => {
+
+    const SignUpController = () => {
+        const signUpService = new SignUpService();
+
+        const signUp = () => {
+            const signUpBtn = document.getElementById("signup-btn");
+            signUpBtn.addEventListener('click', signUpService.signUp);
+        };
+
+        const init = () => {
+            signUp();
+        }
+
+        return {
+            init: init,
+        }
+    };
+
+    const SignUpService = () => {
+        const signUpApi = new SignUpApi();
+
+        if (AppStorage.check('sign-up-running')) return;
+        AppStorage.set('sign-up-running', true);
+
+        const signUp = (event) => {
+            event.preventDefault();
+
+            const email = document.getElementById("login-email-input");
+            const password = document.getElementById("login-password-input");
+            const userName = document.getElementById("login-user-name-input");
+
+            const data = {
+                email: email.value,
+                userName: userName.value,
+                password: password.value,
+            };
+
+            signUpApi.signUp(data)
+                .then(response => {
+                    return response.json();
+                }).then(json => {
+                if (json.hasOwnProperty('errorMessage')) {
+                    alert(json.errorMessage);
+                } else {
+                    //TODO: Redirect login page
+                    alert('가입 완료! 로그인 하세요.');
+                }
+                AppStorage.set('sign-up-running', false);
+            })
+        }
+
+        return {
+            signUp: signUp,
+        }
+    }
+
+    const SignUpApi = () => {
+        const signUp = (data) => {
+            return Api.post('/api/users/signup', data);
+        }
+
+        return {
+            signUp: signUp,
+        }
+    }
+
+    const init = () => {
+        const signUpController = new SignUpController();
+        signUpController.init();
+    };
+
+    return {
+        init: init,
+    };
+})();
+
+SignUpApp.init();
