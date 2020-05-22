e2ea711ddba4611338b86de755bec1814a7213ce
Seongmo Jeon <oeeen3@gmail.com>
2020-05-22 23:02:17 +0900

    chore: add constructor in PreAuthorizationToken
    using UserLoginRequest

--- src/main/java/dev/smjeon/commerce/security/token/PreAuthorizationToken.java
+++ src/main/java/dev/smjeon/commerce/security/token/PreAuthorizationToken.java
@@ -2,12 +2,17 @@ package dev.smjeon.commerce.security.token;
 
 import dev.smjeon.commerce.user.domain.Email;
 import dev.smjeon.commerce.user.domain.Password;
+import dev.smjeon.commerce.user.dto.UserLoginRequest;
 import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
 
 public class PreAuthorizationToken extends UsernamePasswordAuthenticationToken {
 
-    public PreAuthorizationToken(Email email, Password password) {
+    private PreAuthorizationToken(Email email, Password password) {
         super(email, password);
+    }
+
+    public PreAuthorizationToken(UserLoginRequest userLoginRequest) {
+        this(userLoginRequest.getEmail(), userLoginRequest.getPassword());
     }
 
     public Email getEmail() {
