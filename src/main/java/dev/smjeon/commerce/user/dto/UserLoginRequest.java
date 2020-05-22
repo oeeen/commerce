1e3768814d4aea47992a79f690f21fa589947a96
Seongmo Jeon <oeeen3@gmail.com>
2020-05-22 22:58:41 +0900

    chore: add no args constructor in dtos

--- src/main/java/dev/smjeon/commerce/user/dto/UserLoginRequest.java
+++ src/main/java/dev/smjeon/commerce/user/dto/UserLoginRequest.java
@@ -2,12 +2,15 @@ package dev.smjeon.commerce.user.dto;
 
 import dev.smjeon.commerce.user.domain.Email;
 import dev.smjeon.commerce.user.domain.Password;
+import lombok.AccessLevel;
 import lombok.Getter;
+import lombok.NoArgsConstructor;
 
+@NoArgsConstructor(access = AccessLevel.PRIVATE)
 @Getter
 public class UserLoginRequest {
-    private final Email email;
-    private final Password password;
+    private Email email;
+    private Password password;
 
     public UserLoginRequest(Email email, Password password) {
         this.email = email;
--- src/main/java/dev/smjeon/commerce/user/dto/UserResponse.java
+++ src/main/java/dev/smjeon/commerce/user/dto/UserResponse.java
@@ -2,11 +2,20 @@ package dev.smjeon.commerce.user.dto;
 
 import dev.smjeon.commerce.user.domain.Email;
 import dev.smjeon.commerce.user.domain.NickName;
+import lombok.AccessLevel;
 import lombok.Getter;
+import lombok.NoArgsConstructor;
 
+@NoArgsConstructor(access = AccessLevel.PRIVATE)
 @Getter
 public class UserResponse {
     private Email email;
     private String name;
     private NickName nickName;
+
+    public UserResponse(Email email, String name, NickName nickName) {
+        this.email = email;
+        this.name = name;
+        this.nickName = nickName;
+    }
 }
