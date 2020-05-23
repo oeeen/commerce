f6a171345a586b1d6e5e18228c37a0199443b806
Seongmo Jeon <oeeen3@gmail.com>
2020-05-23 21:47:48 +0900

    feat: register login interceptor in web configuration

--- src/main/java/dev/smjeon/commerce/support/WebConfig.java
+++ src/main/java/dev/smjeon/commerce/support/WebConfig.java
@@ -6,12 +6,23 @@ import org.springframework.context.annotation.Configuration;
 import org.springframework.core.annotation.Order;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.web.servlet.config.annotation.InterceptorRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+import org.springframework.web.servlet.handler.HandlerInterceptorAdapter;
+
+import java.util.Arrays;
+import java.util.List;
 
 import static org.modelmapper.config.Configuration.AccessLevel.PRIVATE;
 
 @Configuration
 @Order(1)
-public class WebConfig {
+public class WebConfig implements WebMvcConfigurer {
+    private final HandlerInterceptorAdapter interceptor;
+
+    public WebConfig(HandlerInterceptorAdapter interceptor) {
+        this.interceptor = interceptor;
+    }
 
     @Bean
     public ModelMapper modelMapper() {
@@ -26,5 +37,13 @@ public class WebConfig {
     @Bean
     public PasswordEncoder getPasswordEncoder() {
         return new BCryptPasswordEncoder();
+    }
+
+    @Override
+    public void addInterceptors(InterceptorRegistry registry) {
+        List<String> patterns = Arrays.asList("/", "/api/users/signin", "/api/users/signup", "/js/**", "/css/**", "/images/**");
+        registry.addInterceptor(this.interceptor)
+                .excludePathPatterns(patterns)
+                .addPathPatterns("/**");
     }
 }
