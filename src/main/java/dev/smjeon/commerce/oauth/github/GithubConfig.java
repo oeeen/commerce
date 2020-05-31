f052e4bf27fe640426c8362713604c9f72951376
Seongmo Jeon <oeeen3@gmail.com>
2020-06-01 01:42:49 +0900

    chore: add setter, change prefix in github configuration

--- src/main/java/dev/smjeon/commerce/oauth/github/GithubConfig.java
+++ src/main/java/dev/smjeon/commerce/oauth/github/GithubConfig.java
@@ -1,13 +1,16 @@
 package dev.smjeon.commerce.oauth.github;
 
 import lombok.Getter;
+import lombok.Setter;
 import org.springframework.boot.context.properties.ConfigurationProperties;
 import org.springframework.context.annotation.Configuration;
 
-@Getter
 @Configuration
-@ConfigurationProperties(prefix = "naver.client")
+@ConfigurationProperties(prefix = "github.client")
+@Getter
+@Setter
 public class GithubConfig {
     private String clientId;
     private String clientSecret;
+    private String redirectUrl;
 }
