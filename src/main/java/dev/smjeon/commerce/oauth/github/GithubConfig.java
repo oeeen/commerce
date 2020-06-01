cd97069a400813367abdd20b49868b65cb232171
Seongmo Jeon <oeeen3@gmail.com>
2020-06-01 18:11:25 +0900

    chore: add authorization url

--- src/main/java/dev/smjeon/commerce/oauth/github/GithubConfig.java
+++ src/main/java/dev/smjeon/commerce/oauth/github/GithubConfig.java
@@ -12,5 +12,6 @@ import org.springframework.context.annotation.Configuration;
 public class GithubConfig {
     private String clientId;
     private String clientSecret;
+    private String authorizationUrl;
     private String redirectUrl;
 }
