77f0d9dcbe23d71165eba77424fde8ed82cb99de
Seongmo Jeon <oeeen3@gmail.com>
2020-06-11 23:23:29 +0900

    chore: add application.yml in applicationContext

--- src/main/java/dev/smjeon/commerce/CommerceApplication.java
+++ src/main/java/dev/smjeon/commerce/CommerceApplication.java
@@ -6,10 +6,11 @@ import org.springframework.boot.builder.SpringApplicationBuilder;
 @SpringBootApplication
 public class CommerceApplication {
 	private static final String OAUTH = "spring.config.location=classpath:oauth.yml";
+	private static final String CONFIG = "spring.config.location=classpath:application.yml";
 
 	public static void main(String[] args) {
 		new SpringApplicationBuilder(CommerceApplication.class)
-				.properties(OAUTH)
+				.properties(OAUTH, CONFIG)
 				.run(args);
 	}
 
