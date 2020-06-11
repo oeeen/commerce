0cc320f8f61a1acdbabc683a16f8477ee8aa99d9
Seongmo Jeon <oeeen3@gmail.com>
2020-06-11 17:33:06 +0900

    feat: apply authority restrictions in api call

--- src/main/java/dev/smjeon/commerce/security/config/SecurityConfig.java
+++ src/main/java/dev/smjeon/commerce/security/config/SecurityConfig.java
@@ -5,11 +5,14 @@ import dev.smjeon.commerce.security.filters.GithubLoginFilter;
 import dev.smjeon.commerce.security.filters.KakaoLoginFilter;
 import dev.smjeon.commerce.security.providers.FormLoginAuthenticationProvider;
 import dev.smjeon.commerce.security.providers.SocialLoginAuthenticationProvider;
+import dev.smjeon.commerce.user.domain.UserRole;
+import org.springframework.boot.autoconfigure.security.servlet.PathRequest;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
 import org.springframework.security.config.annotation.method.configuration.EnableGlobalMethodSecurity;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.builders.WebSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
 import org.springframework.security.web.authentication.AuthenticationFailureHandler;
@@ -64,6 +67,15 @@ public class SecurityConfig extends WebSecurityConfigurerAdapter {
     }
 
     @Override
+    public void configure(WebSecurity web) throws Exception {
+        web
+                .ignoring().requestMatchers(PathRequest.toStaticResources().atCommonLocations());
+
+        web
+                .ignoring().requestMatchers(PathRequest.toH2Console());
+    }
+
+    @Override
     protected void configure(AuthenticationManagerBuilder auth) throws Exception {
         auth
                 .authenticationProvider(this.formLoginAuthenticationProvider)
@@ -75,7 +87,11 @@ public class SecurityConfig extends WebSecurityConfigurerAdapter {
     protected void configure(HttpSecurity http) throws Exception {
         http
                 .authorizeRequests()
-                .anyRequest().permitAll();
+                .antMatchers("/", "/api/users/signin", "/api/users/signup", "/login", "/login/**", "/signup").permitAll()
+                .antMatchers("/api/products/**").permitAll()
+                .antMatchers("/api/users").hasRole(UserRole.ADMIN.name())
+                .anyRequest().authenticated();
+
         http
                 .csrf().ignoringAntMatchers("/h2-console", "/h2-console**", "/h2-console/", "/h2-console/**", "/api/users/**")
                 .and()
