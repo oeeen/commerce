6447b12ba35b0a121c41e6b953a4fdf6347ae698
Seongmo Jeon <oeeen3@gmail.com>
2020-06-14 03:05:29 +0900

    feat: add AccessDeniedHandler, Controller

---
+++ src/main/java/dev/smjeon/commerce/security/handlers/CustomAccessDeniedHandler.java
@@ -0,0 +1,18 @@
+package dev.smjeon.commerce.security.handlers;
+
+import org.springframework.security.access.AccessDeniedException;
+import org.springframework.security.web.access.AccessDeniedHandler;
+
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+
+public class CustomAccessDeniedHandler implements AccessDeniedHandler {
+
+    @Override
+    public void handle(HttpServletRequest request, HttpServletResponse response, AccessDeniedException accessDeniedException) throws IOException, ServletException {
+        String deniedUrl = "/denied?exception=" + accessDeniedException.getMessage();
+        response.sendRedirect(deniedUrl);
+    }
+}
