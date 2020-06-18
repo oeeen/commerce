16303487f4aae8c7f6c5a9f2848f3cda3db00e12
Seongmo Jeon <oeeen3@gmail.com>
2020-06-19 00:03:00 +0900

    test: add product entity test

--- src/test/java/dev/smjeon/commerce/product/domain/ProductTest.java
+++ src/test/java/dev/smjeon/commerce/product/domain/ProductTest.java
@@ -10,46 +10,89 @@ import dev.smjeon.commerce.user.domain.Password;
 import dev.smjeon.commerce.user.domain.User;
 import dev.smjeon.commerce.user.domain.UserRole;
 import dev.smjeon.commerce.user.domain.UserStatus;
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 
 import static org.junit.jupiter.api.Assertions.assertDoesNotThrow;
+import static org.junit.jupiter.api.Assertions.assertEquals;
 
 public class ProductTest {
+    private TopCategory topCategory;
+    private ProductName productName;
+    private ProductType productType;
+    private Price price;
+    private ShippingFee shippingFee;
+    private User owner;
 
-    @Test
-    @DisplayName("카테고리, 상품 타입, 상품명, 기본 가격, 재고 수, 기본 배송비, 등록자로 상품을 등록합니다.")
-    void constructProduct() {
-        CategoryName topCategoryName = new CategoryName("식품");
-        CategoryName subCategoryName = new CategoryName("신선식품");
-        CategoryName lowestCategoryName = new CategoryName("쌀");
-        LowestCategory lowestCategory = new LowestCategory(lowestCategoryName);
-        SubCategory subCategory = new SubCategory(subCategoryName, lowestCategory);
-
-        ProductName productName = new ProductName("농협", "파주참드림_10KG 포");
-
-        ProductType productType = ProductType.NORMAL;
-
-        Price price = new Price(10_000);
-
-        ShippingFee shippingFee = new ShippingFee(2_500);
-
-        User owner = new User(
+    @BeforeEach
+    void setUp() {
+        owner = new User(
                 new Email("oeeen3@gmail.com"),
                 new Password("Aa12345!"),
                 "Seongmo",
                 new NickName("Martin"),
                 UserRole.SELLER,
                 UserStatus.ACTIVE);
+        CategoryName topCategoryName = new CategoryName("식품");
+        CategoryName subCategoryName = new CategoryName("신선식품");
+        CategoryName lowestCategoryName = new CategoryName("쌀");
+        LowestCategory lowestCategory = new LowestCategory(lowestCategoryName);
+        SubCategory subCategory = new SubCategory(subCategoryName, lowestCategory);
+        topCategory = new TopCategory(topCategoryName, subCategory);
+        productName = new ProductName("농협", "파주참드림_10KG 포");
+        productType = ProductType.NORMAL;
+        price = new Price(10_000);
+        shippingFee = new ShippingFee(2_500);
+    }
 
+    @Test
+    @DisplayName("카테고리, 상품 타입, 상품명, 기본 가격, 재고 수, 기본 배송비, 등록자로 상품을 등록합니다.")
+    void constructProduct() {
         assertDoesNotThrow(() ->
                 new Product(
-                        new TopCategory(topCategoryName, subCategory),
+                        topCategory,
                         productName,
                         productType,
                         price,
                         shippingFee,
                         owner,
                         ProductStatus.ACTIVE));
+    }
+
+    @Test
+    @DisplayName("동일 유저일 경우 ProductStatus가 REMOVED로 변경됩니다.")
+    void remove() {
+        Product product = new Product(
+                topCategory,
+                productName,
+                productType,
+                price,
+                shippingFee,
+                owner,
+                ProductStatus.ACTIVE
+        );
+
+        product.remove(owner);
+
+        assertEquals(product.getStatus(), ProductStatus.REMOVED);
+    }
+
+    @Test
+    @DisplayName("ProductStatus가 BLOCKED로 변경됩니다.")
+    void removeByAdmin() {
+        Product product = new Product(
+                topCategory,
+                productName,
+                productType,
+                price,
+                shippingFee,
+                owner,
+                ProductStatus.ACTIVE
+        );
+
+        product.block();
+
+        assertEquals(product.getStatus(), ProductStatus.BLOCKED);
     }
 }
